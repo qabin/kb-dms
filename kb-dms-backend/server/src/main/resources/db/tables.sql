104cb36d187393b1d902bda11266f921962893bd
kongbin02 <kongbin02@ppdai.com>
2020-02-13 15:28:18 +0800

    init

--- kb-dms-backend/server/src/main/resources/db/tables.sql
+++ kb-dms-backend/server/src/main/resources/db/tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `user_info`(
+CREATE TABLE `kb-dms`.`user_info`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `account` VARCHAR(256) NOT NULL COMMENT '登录账号',
     `login_pwd` VARCHAR(256) NOT NULL COMMENT '登录密码',
@@ -9,7 +9,7 @@ CREATE TABLE `user_info`(
     UNIQUE KEY `login_name_UNIQUE` (`account`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息表';
 
-CREATE TABLE `us_sql_editor_tab`(
+CREATE TABLE `kb-dms`.`us_sql_editor_tab`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(1024),
     `db` VARCHAR(256) NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE `us_sql_editor_tab`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户窗口表';
 
-CREATE TABLE `us_favorite_table`(
+CREATE TABLE `kb-dms`.`us_favorite_table`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `datasource_id` INT(11) NOT NULL,
     `db` VARCHAR(1024) NOT NULL,
@@ -36,7 +36,7 @@ CREATE TABLE `us_favorite_table`(
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户收藏表';
 
 
-CREATE TABLE `us_favorite_group`(
+CREATE TABLE `kb-dms`.`us_favorite_group`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `group_id` INT(11) NOT NULL,
     `create_time` DATETIME NOT NULL,
@@ -44,7 +44,7 @@ CREATE TABLE `us_favorite_group`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
-CREATE TABLE `us_favorite_db`(
+CREATE TABLE `kb-dms`.`us_favorite_db`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `datasource_id` INT(11) NOT NULL,
     `db` VARCHAR(1024) NOT NULL,
@@ -53,7 +53,7 @@ CREATE TABLE `us_favorite_db`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
-CREATE TABLE `us_favorite_datasource`(
+CREATE TABLE `kb-dms`.`us_favorite_datasource`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `datasource_id` INT(11) NOT NULL,
     `account` VARCHAR(1024) NOT NULL,
@@ -62,7 +62,7 @@ CREATE TABLE `us_favorite_datasource`(
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
 
-CREATE TABLE `us_active_sql_editor_tab`(
+CREATE TABLE `kb-dms`.`us_active_sql_editor_tab`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `account` VARCHAR(256) NOT NULL,
     `create_time` DATETIME NOT NULL,
@@ -71,7 +71,7 @@ CREATE TABLE `us_active_sql_editor_tab`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
-CREATE TABLE `st_sql_syntax_error_count_timeline`(
+CREATE TABLE `kb-dms`.`st_sql_syntax_error_count_timeline`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `type` INT(11) NOT NULL COMMENT '1. 全部 2. 缺少limit 3. 缺少where 4. 越权',
     `count` INT(11) NOT NULL,
@@ -79,7 +79,7 @@ CREATE TABLE `st_sql_syntax_error_count_timeline`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
-CREATE TABLE `st_sql_syntax_error_count_day`(
+CREATE TABLE `kb-dms`.`st_sql_syntax_error_count_day`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `type` INT(11) NOT NULL COMMENT '1. 缺少limit 2. 缺少where 3. 越权',
     `count` INT(11) NOT NULL,
@@ -87,7 +87,7 @@ CREATE TABLE `st_sql_syntax_error_count_day`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
-CREATE TABLE `st_sql_exe_result_count_timeline`(
+CREATE TABLE `kb-dms`.`st_sql_exe_result_count_timeline`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `type` INT(11) NOT NULL COMMENT '1. 全部 2. 执行成功 3. 执行失败 4. 执行中',
     `count` INT(11) NOT NULL,
@@ -95,7 +95,7 @@ CREATE TABLE `st_sql_exe_result_count_timeline`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
-CREATE TABLE `st_sql_exe_result_count_day`(
+CREATE TABLE `kb-dms`.`st_sql_exe_result_count_day`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `type` INT(11) NOT NULL COMMENT '1. 全部 2. 执行成功 3. 执行失败 4. 执行中',
     `count` INT(11) NOT NULL,
@@ -103,7 +103,7 @@ CREATE TABLE `st_sql_exe_result_count_day`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
-CREATE TABLE `st_sql_exe_biz_total`(
+CREATE TABLE `kb-dms`.`st_sql_exe_biz_total`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `sql_exe_result_total` INT(11) NOT NULL DEFAULT '0',
     `sql_exe_result_success_total` INT(11) NOT NULL DEFAULT '0',
@@ -116,7 +116,7 @@ CREATE TABLE `st_sql_exe_biz_total`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
-CREATE TABLE `rs_sql_exe_result`(
+CREATE TABLE `kb-dms`.`rs_sql_exe_result`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `sql_exe_record_id` INT(11) NOT NULL,
     `sql_text` LONGTEXT NOT NULL,
@@ -139,7 +139,7 @@ CREATE TABLE `rs_sql_exe_result`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
-CREATE TABLE `rs_sql_exe_record`(
+CREATE TABLE `kb-dms`.`rs_sql_exe_record`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `sql_text` LONGTEXT NOT NULL,
     `datasource_id` INT(11) NOT NULL,
@@ -152,7 +152,7 @@ CREATE TABLE `rs_sql_exe_record`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
-CREATE TABLE `cf_datasource_permission_sql_options`(
+CREATE TABLE `kb-dms`.`cf_datasource_permission_sql_options`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `datasource_id` INT(11) NOT NULL,
     `option_type` INT(11) NOT NULL,
@@ -160,7 +160,7 @@ CREATE TABLE `cf_datasource_permission_sql_options`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
-CREATE TABLE `cf_datasource_permission_member`(
+CREATE TABLE `kb-dms`.`cf_datasource_permission_member`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `datasource_id` INT(11) NOT NULL,
     `account` VARCHAR(256) NOT NULL,
@@ -168,7 +168,7 @@ CREATE TABLE `cf_datasource_permission_member`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
-CREATE TABLE `cf_datasource_owners`(
+CREATE TABLE `kb-dms`.`cf_datasource_owners`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `datasource_id` INT(11) NOT NULL,
     `name` VARCHAR(256) NOT NULL,
@@ -176,7 +176,7 @@ CREATE TABLE `cf_datasource_owners`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
-CREATE TABLE `cf_datasource`(
+CREATE TABLE `kb-dms`.`cf_datasource`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `group_id` INT(11) NOT NULL,
     `name` VARCHAR(256) NOT NULL,
@@ -196,7 +196,7 @@ CREATE TABLE `cf_datasource`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
-CREATE TABLE `cf_bus_module`(
+CREATE TABLE `kb-dms`.`cf_bus_module`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(45),
     `group_id` INT(11),
@@ -206,7 +206,7 @@ CREATE TABLE `cf_bus_module`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
-CREATE TABLE `cf_bus_group_users`(
+CREATE TABLE `kb-dms`.`cf_bus_group_users`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `bus_group_id` INT(11) NOT NULL,
     `name` VARCHAR(256) NOT NULL,
@@ -216,7 +216,7 @@ CREATE TABLE `cf_bus_group_users`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
-CREATE TABLE `cf_bus_group_owners`(
+CREATE TABLE `kb-dms`.`cf_bus_group_owners`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(256) NOT NULL,
     `account` VARCHAR(256) NOT NULL,
@@ -224,7 +224,7 @@ CREATE TABLE `cf_bus_group_owners`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
-CREATE TABLE `cf_bus_group`(
+CREATE TABLE `kb-dms`.`cf_bus_group`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(256) NOT NULL COMMENT '创建人',
     `create_time` DATETIME NOT NULL,
@@ -235,7 +235,7 @@ CREATE TABLE `cf_bus_group`(
     KEY `idx_id_name` (`id`,`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='sf';
 
-CREATE TABLE `cf_admin_config`(
+CREATE TABLE `kb-dms`.`cf_admin_config`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `account` VARCHAR(256) NOT NULL,
     `create_time` DATETIME NOT NULL,
