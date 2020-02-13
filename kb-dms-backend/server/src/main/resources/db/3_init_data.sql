ecb75c4990e84773c85191c635bcb4e18d57f94b
kongbin02 <kongbin02@ppdai.com>
2020-02-13 16:36:44 +0800

    init

---
+++ kb-dms-backend/server/src/main/resources/db/3_init_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `kb-dms`.`user_info` (`id`,`account`,`login_pwd`,`name`,`create_time`,`update_time`) VALUES ('1','admin','21232f297a57a5a743894a0e4a801fc3','管理员','2020-01-01 00:00:00','2020-01-01 00:00:00');
+
+INSERT INTO `kb-dms`.`cf_admin_config` (`id`,`account`,`create_time`,`creator_name`,`creator_account`,`name`) VALUES ('1','admin','2020-01-01 00:00:00','admin','admin','管理员');
